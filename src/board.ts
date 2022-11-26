5d9e2efe2fa9cd4c43e9b5b878d67d7693c38e17
Yohan <25325031+sparepillowgit@users.noreply.github.com>
2022-11-26 20:33:57 +1000

    feat: Complete basic board generation

--- src/board.ts
+++ src/board.ts
@@ -2,12 +2,21 @@
 module.exports = {
     create: function () {
         interface Square {
-            occupyingPiece: Piece;
+            occupyingPieceId: number | null;
         }
 
-        let board: Record<string, Record<string, Square>> = {
-            'a': { '1': { occupyingPiece: {} } },
-        };
+        const board: Record<string, Record<string, Square>> = {};
+        const ranks: Record<string, Square> = {};
+
+        // Create ranks to append to board[file]. Doing this here so that
+        // I don't have to run a nested for loop for each file.
+        ['1', '2', '3', '4', '5', '6', '7', '8'].forEach(rank => {
+            ranks[rank] = {occupyingPieceId: null};
+        });
+
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].forEach(file => {
+            board[file] = ranks;
+        });
 
         return board;
     }
