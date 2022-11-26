1027154d1a8a87e4fbf34b25815c88d0cba3a4b0
Yohan <25325031+sparepillowgit@users.noreply.github.com>
2022-11-27 09:26:34 +1000

    chore: Update board generation

--- src/board.ts
+++ src/board.ts
@@ -1,23 +1,35 @@
+interface Square {
+	occupyingPieceId: number | null;
+}
+
+interface Board {
+	squares: Record<string, Record<string, Square>>
+}
+
+function generateSquares(): Record<string, Record<string, Square>> {
+	const squares: Record<string, Record<string, Square>> = {};
+	const ranks: Record<string, Square> = {};
+
+	// Create ranks to append to board[file]. I'm doing this here so
+	// I don't have to run a nested for-loop for each file.
+
+	['1', '2', '3', '4', '5', '6', '7', '8'].forEach(rank => {
+		ranks[rank] = {occupyingPieceId: null};
+	});
+
+	['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].forEach(file => {
+		squares[file] = ranks;
+	});
+
+	return squares;
+}
 
 module.exports = {
-    create: function () {
-        interface Square {
-            occupyingPieceId: number | null;
-        }
+	create: function (): Board {
+		const board: Board = {
+			squares: generateSquares()
+		}
 
-        const board: Record<string, Record<string, Square>> = {};
-        const ranks: Record<string, Square> = {};
-
-        // Create ranks to append to board[file]. Doing this here so that
-        // I don't have to run a nested for loop for each file.
-        ['1', '2', '3', '4', '5', '6', '7', '8'].forEach(rank => {
-            ranks[rank] = {occupyingPieceId: null};
-        });
-
-        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].forEach(file => {
-            board[file] = ranks;
-        });
-
-        return board;
-    }
+		return board;
+	}
 };
